2d70558e7813cffbcdbc92dd99d450668d6617c9
Ralf Hannuschka <r.hannuschka@gmail.com>
2023-07-24 22:33:12 +0200

    chore(@trueffelmafia/electron-typings): 0.0.8

--- src/services/ApplicationService.ts
+++ src/services/ApplicationService.ts
@@ -1,4 +1,4 @@
-import type { Application } from '@trueffelmafia/electron-types'
+import type { Application, Command } from '@trueffelmafia/electron-types'
 import { final } from '../decorators/final'
 
 @final
@@ -7,26 +7,26 @@ export class ApplicationService {
   /**
    * execute command to open application window
    */
-  static open<T = unknown>(body: Application.OpenRequestParam<T>): Promise<Application.OpenResponse> {
+  static open<T = unknown>(payload: Application.OpenRequestParam<T>): Command.Response<Application.ApplicationDTO> {
     const req: Application.OpenRequest<T> = {
       command: 'application:open',
-      body 
+      payload
     }
-    return window.tm_electron.exec<Application.OpenResponse>(req)
+    return window.tm_electron.exec(req)
   }
 
   /**
    * execute command to close an application window
    */
-  static close(id: string): Promise<void> {
+  static close(id: string): Command.Response<void> {
     const request: Application.CloseRequest = {
       command: 'application:close',
-      body: { id }
+      payload: { id }
     }
-    return window.tm_electron.exec<void>(request)
+    return window.tm_electron.exec(request)
   }
 
-  static resolveState(): Promise<Application.State> {
+  static resolveState(): Command.Response<Application.State> {
     const request: Application.StateRequest = {
       command: 'application:state',
     }
