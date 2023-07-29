00325649cf548ec04af5ec48882d408ab2c0093e
Ralf Hannuschka <r.hannuschka@gmail.com>
2023-07-30 01:42:37 +0200

    add message service

--- src/services/MessageService.ts
+++ src/services/MessageService.ts
@@ -1,9 +1,14 @@
+import type { Observable } from 'rxjs'
 import type { Message } from "@trueffelmafia/electron-types"
+import { map } from 'rxjs/operators'
 import { final } from "../decorators/final"
 
 @final
 export class MessageService {
 
+  /**
+   * @description sends a message to main process
+   */
   send<T = unknown>(broker: string, channel: string, action: string, payload?: T): void {
     const data: Message.RendererMessage<T> = {
       path: `${broker}:${channel}:${action}`,
@@ -11,5 +16,15 @@ export class MessageService {
     }
 
     window.tm_electron.send(data)
+  }
+
+  /**
+   * @description received a message from main process
+   */
+  message<R = unknown>(): Observable<Message.MainProcessMessage<R>> {
+    return window.tm_electron.received<R>().pipe(
+      map(([, message]) => message)
+      // TODO add filter
+    )
   }
 }
