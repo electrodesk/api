70781175cc782aae22d28d9a4a0f101cefcf170b
Ralf Hannuschka <r.hannuschka@gmail.com>
2023-10-22 23:32:31 +0200

    fix: application facade fetches all broadcast events even from other … (#12)
    * fix: application facade fetches all broadcast events even from other applications
    
    * fix: remove merge conflict markers
    
    ---------
    
    Co-authored-by: Ralf Hannuschka (adesso) <ralf.hannuschka@adesso.de>

--- src/services/MessageService.ts
+++ src/services/MessageService.ts
@@ -1,44 +1,30 @@
-import { ApplicationExecCommand, ApplicationReadDTO } from "@electrodesk/types/application"
-import { CommandHandlerParam, ElectronEvent, EventHandlerParam } from "@electrodesk/types/core"
-import { final } from "../decorators/final"
-import { CommandContainer } from "../model/Command.container"
+import { ApplicationEvent } from "@electrodesk/types/application";
+import { CommandHandlerParam, ElectronEvent } from "@electrodesk/types/core";
+import { final } from "../decorators/final";
+import { CommandContainer } from "../model/Command.container";
 
-export declare type CommandListener = (command: CommandContainer) => void
-export declare type EventListener = (payload: unknown, event: string) => void
-declare type MessageRegistry<Listener = CommandListener | EventListener> = Map<string, Listener[]>
+export declare type CommandListener = (command: CommandContainer) => void;
+export declare type EventListener = (payload: unknown, event: string) => void;
+declare type MessageRegistry<Listener = CommandListener | EventListener> = Map<
+  string,
+  Listener[]
+>;
 
 @final
 export class MessageService {
-
-  public commandListenerRegistry: MessageRegistry<CommandListener> = new Map()
-  public eventListenerRegistry: MessageRegistry<EventListener> = new Map()
+  public commandListenerRegistry: MessageRegistry<CommandListener> = new Map();
+  public eventListenerRegistry: MessageRegistry<EventListener> = new Map();
 
   clear(): void {
-    this.commandListenerRegistry.clear()
-    this.eventListenerRegistry.clear()
+    this.commandListenerRegistry.clear();
+    this.eventListenerRegistry.clear();
   }
 
   /**
    * @description emit event through renderer process
    */
   dispatchEvent(event: ElectronEvent): void {
-    window.electrodesk.dispatchEvent(event)
-  }
-
-  /**
-   * @description sends command to another application. Like open file or stop playing
-   * this video
-   */
-  exec<T = unknown>(application: ApplicationReadDTO['uuid'], commandName: string, commandPayload?: T): void {
-    const command: ApplicationExecCommand<T> = {
-      command: 'application:exec',
-      applicationId: application,
-      payload: {
-        command: commandName,
-        data: commandPayload
-      }
-    }
-    window.electrodesk.execCommand<T>(command)
+    window.electrodesk.dispatchEvent(event);
   }
 
   /**
@@ -46,21 +32,21 @@ export class MessageService {
    */
   attachCommandListener(command: string, listener: CommandListener): void {
     if (this.commandListenerRegistry.size === 0) {
-      window.electrodesk.addCommandHandler(this.handleCommand.bind(this))
+      window.electrodesk.addCommandHandler(this.handleCommand.bind(this));
     }
-    this.attachListener(this.commandListenerRegistry, command, listener)
+    this.attachListener(this.commandListenerRegistry, command, listener);
   }
 
   /**
    * remove listener from specific command
-   * 
+   *
    * @todo improve typings, we get the information it should be a MessageRegistry<CommandListener> but
    * we can also pass MessageRegistry<EventListener> and it is also fine but it should't
    */
   detachCommandListener(command: string, listener: CommandListener): void {
-    this.detachListener(this.commandListenerRegistry, command, listener)
+    this.detachListener(this.commandListenerRegistry, command, listener);
     if (this.commandListenerRegistry.size === 0) {
-      window.electrodesk.removeCommandHandler(this.handleCommand.bind(this))
+      window.electrodesk.removeCommandHandler(this.handleCommand.bind(this));
     }
   }
 
@@ -69,69 +55,81 @@ export class MessageService {
    */
   attachEventListener(event: string, listener: EventListener): void {
     if (this.eventListenerRegistry.size === 0) {
-      window.electrodesk.addEventHandler(this.handleEvent.bind(this))
+      window.electrodesk.addEventHandler(this.handleEvent.bind(this));
     }
-    this.attachListener(this.eventListenerRegistry, event, listener)
-  } 
+    this.attachListener(this.eventListenerRegistry, event, listener);
+  }
 
   /**
    * detach listener from specific event
    */
   detachEventListener(event: string, listener: EventListener): void {
-    this.detachListener(this.eventListenerRegistry, event, listener)
+    this.detachListener(this.eventListenerRegistry, event, listener);
     if (this.eventListenerRegistry.size === 0) {
-      window.electrodesk.removeEventHandler(this.handleEvent.bind(this))
+      window.electrodesk.removeEventHandler(this.handleEvent.bind(this));
     }
-  } 
+  }
 
   /**
    * attach listener to registry
    */
-  private attachListener<Listener = CommandListener | EventListener>(registry: MessageRegistry<Listener>, name: string, listener: Listener): void {
+  private attachListener<Listener = CommandListener | EventListener>(
+    registry: MessageRegistry<Listener>,
+    name: string,
+    listener: Listener
+  ): void {
     if (!registry.has(name)) {
-      registry.set(name, [listener])
-      return
+      registry.set(name, [listener]);
+      return;
     }
-    const listeners = registry.get(name) as Listener[]
-    registry.set(name, [...listeners, listener])
+    const listeners = registry.get(name) as Listener[];
+    registry.set(name, [...listeners, listener]);
   }
 
   /**
    * remove listener from registry, if no listener left entry will completly removed from map
    */
-  private detachListener<Listener = CommandListener | EventListener>(registry: MessageRegistry<Listener>, name: string, listener: Listener): void {
+  private detachListener<Listener = CommandListener | EventListener>(
+    registry: MessageRegistry<Listener>,
+    name: string,
+    listener: Listener
+  ): void {
     if (!registry.has(name)) {
-      return
+      return;
     }
 
-    const listeners = registry.get(name) as Listener[]
-    const cleanedListeners = listeners.filter((registeredListener) => registeredListener !== listener)
+    const listeners = registry.get(name) as Listener[];
+    const cleanedListeners = listeners.filter(
+      (registeredListener) => registeredListener !== listener
+    );
     if (cleanedListeners.length === 0) {
-      registry.delete(name)
-      return
+      registry.delete(name);
+      return;
     }
-    registry.set(name, cleanedListeners)
+    registry.set(name, cleanedListeners);
   }
 
   private handleCommand(message: CommandHandlerParam): void {
     if (this.commandListenerRegistry.has(message.command)) {
-      const listeners = this.commandListenerRegistry.get(message.command) as CommandListener[]
+      const listeners = this.commandListenerRegistry.get(
+        message.command
+      ) as CommandListener[];
       for (const listener of listeners) {
         const commandContainer = new CommandContainer(
           message.commandId,
-          message.payload,
-        )
-        listener(commandContainer)
+          message.payload
+        );
+        listener(commandContainer);
       }
     }
   }
 
-  private handleEvent(param: EventHandlerParam): void {
-    const listeners = this.eventListenerRegistry.get(param.event) ?? [];
-    const globalListeners = this.eventListenerRegistry.get('*') ?? [];
+  private handleEvent(event: ApplicationEvent): void {
+    const listeners = this.eventListenerRegistry.get(event.name) ?? [];
+    const globalListeners = this.eventListenerRegistry.get("*") ?? [];
     const combinedListeners = [...listeners, ...globalListeners];
     for (const listener of combinedListeners) {
-      listener(param.payload, param.event)
+      listener(event.data, event.name);
     }
   }
 }
